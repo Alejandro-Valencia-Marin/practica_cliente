a0ef41acb59e77e74c8e375c90297404c5cbe6c1
Alejandro Valencia <alexvama20043@gmail.com>
2024-11-11 11:13:28 +0100

    second commit

---
+++ constants.js
@@ -0,0 +1,14 @@
+
+//forma simple
+export const COMPANY = "PIio Baroja"
+export const MODULE = "programacion en entorno cliente"
+
+
+
+// forma Default
+
+export default {
+COMP : "PIio Baroja",
+MOD : "programacion en entorno cliente"
+}
+
--- main.js
+++ main.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+//Forma simple
+
+import { COMPANY, MODULE } from "./constants";
+
+
+console.log("hello world" + COMPANY);
+console.log("hello worls" + MODULE);
+
+
+//forma default
+import constants from "./constants";
+
+console.log("Compañia" + constants.COMPANY)
+console.log("Departamentos " + constants.MODULE)
